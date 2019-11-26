e841cbcb3869534f6394a6191baf7db9c82a09b1
zhangruiliu <1045778389@qq.com>
2019-11-26 14:15:13 +0800

    WCFServer

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstWCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(TeachService));
+            host.Open();
+            Console.WriteLine("WCF SERVICE IS START!");
+            Console.ReadLine();
+        }
+    }
+}
