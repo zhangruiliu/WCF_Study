c4a564a30c2e4462fc6139372f1dde24ad24972f
zhangruiliu <1045778389@qq.com>
2019-11-26 14:18:16 +0800

    WCFClent

---
+++ WCFAPPClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFAPPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyWFCService.TeachServiceClient client = new MyWFCService.TeachServiceClient();
+            string serverName= client.GetServerName();
+            string name=client.GetServerByName("你好");
+            client.Close();
+            client = new MyWFCService.TeachServiceClient();
+            name = client.GetServerByName("你好");
+            Console.WriteLine(serverName);
+            Console.WriteLine(name);
+            Console.ReadLine();
+        }
+    }
+}
