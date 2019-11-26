dd5ac0cb13931fe9eee73279563765963175c827
zhangruiliu <1045778389@qq.com>
2019-11-26 16:53:47 +0800

    tcpservice

---
+++ TCPWCFClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPWCFClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TCPWCFService.TCPTeachServiceClient client = new TCPWCFService.TCPTeachServiceClient();
+            string name= client.GetServerName();
+            Console.WriteLine(name);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TCPWCFServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPWCFServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(TCPTeachService));
+            host.Open();
+            Console.Read();
+        }
+    }
+}
